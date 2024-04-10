c59ffa3bf840e705eb54afd1890ee17d5adb35d7
poojanoza <poojanoza89@gmail.com>
2024-04-11 01:41:28 +0530

    Added operator Funcanality.

--- Calculater/index.js
+++ Calculater/index.js
@@ -1,9 +1,6 @@
-// const b7=document.getElementById("b7").textContent
-
-// document.getElementById("display").innerHTML= b7
-
 let currentNum="";
-let PreNum="";
+let PreviousNum="";
+let operator="";
 
 function addNumber(num){
     currentNum+=num;
@@ -11,6 +8,58 @@ function addNumber(num){
 }
 
 function clearDisplay(){
-    currentNum="0";
+    currentNum="";
+    PreviousNum=""
+    operator="";
     document.getElementById("display").value =currentNum;
+}
+
+
+function setOp(op){
+    if(currentNum==="") return;
+    PreviousNum=currentNum;
+    currentNum="";
+    operator=op;
+    const display_value =document.getElementById("display").value
+    if(display_value.value !== "" || display_value.includes(op)){
+    document.getElementById("display").value = display_value+op;
+    }
+}
+
+function cal(){
+    if(operator==="") return;
+
+    let a =0;
+    switch(operator){
+        
+        case '+':
+        a= parseFloat(PreviousNum)+parseFloat(currentNum);
+        break;
+
+        case '-':
+        a= parseFloat(PreviousNum)-parseFloat(currentNum);
+        break;
+
+        case '*':
+        a= parseFloat(PreviousNum)*parseFloat(currentNum);
+        break;
+
+        case '/':
+        if(currentNum === "0"){
+            alert("Error Division by 0");
+            return;
+        }
+        a= parseFloat(PreviousNum)/parseFloat(currentNum);
+        break;
+    }
+
+    PreviousNum = a.toString();
+    currentNum="";
+    document.getElementById("display").value =a;
+}
+
+function onebyone(){
+    if(currentNum == ""){ return;}
+    currentNum = currentNum.slice(0,-1);
+    document.getElementById("display").value = currentNum
 }
